ebb95f3032c893137d2e2b475dff0d2acf2a353f
FatemehM84 <fmohammadi22384@gmail.com>
2026-07-05 22:12:07 +0330

    fix the admin page problems

--- acme_backend/settings.py
+++ acme_backend/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 AUTH_USER_MODEL = 'accounts.User'
 
